614bc1bc800ec37c46b8cb764c11b7855b12ae7a
MaxHel <maxime.heliotpro@gmail.com>
2020-12-22 11:21:39 +0100

    Finish challenge 2

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float spawnInterval = 1.0f;
 
+    private bool cooldown = false;
+    
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (cooldown == false)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                Invoke("ResetCooldown", spawnInterval);
+                cooldown = true;
+            }
         }
+    }
+    
+    void ResetCooldown(){
+        cooldown = false;
     }
 }
--- Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -22,11 +22,14 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        spawnInterval = Random.Range(3.0f, 5.0f);
+        
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
