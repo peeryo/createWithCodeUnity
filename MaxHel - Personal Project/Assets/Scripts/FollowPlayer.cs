9b738266265e248227f37fb7a396a00a14585671
MaxHel <maxime.heliotpro@gmail.com>
2020-12-23 15:46:00 +0100

    Add player control

---
+++ MaxHel - Personal Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 6, -5); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ MaxHel - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forwardSpeed = 10.0f;
+    public float turnSpeed = 25.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+            
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up,Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
