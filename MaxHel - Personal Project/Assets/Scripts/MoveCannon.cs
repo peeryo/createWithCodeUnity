b828af358cc74d71bf1e7e268ed243744850968e
MaxHel <maxime.heliotpro@gmail.com>
2020-12-24 16:35:08 +0100

    Update personnal project

---
+++ MaxHel - Personal Project/Assets/Scripts/MoveCannon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MoveCannon : MonoBehaviour
+{
+    public float rotationSpeed = 5.0f;
+    public float rotationLimit = 90;
+    private float rotationX = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationX += Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, 0, rotationLimit);
+
+        transform.localEulerAngles = new Vector3(-rotationX, transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
+}
---
+++ MaxHel - Personal Project/Assets/Scripts/MoveTower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MoveTower : MonoBehaviour
+{
+    public float rotationSpeed = 5.0f;
+    public float rotationLimit = 90;
+    
+    private float rotationY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationY += Input.GetAxis("Mouse X") * rotationSpeed;
+        rotationY = Mathf.Clamp(rotationY, -rotationLimit, rotationLimit);
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, rotationY, transform.localEulerAngles.z);
+    }
+}
